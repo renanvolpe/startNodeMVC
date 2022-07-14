f18a623c73108d0f9db79772e5ebd63b56020f50
Volpe <renan.volpe@aluno.ifsp.edu.br>
2022-07-14 17:22:20 -0300

    list ways to show datas

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
+import{Op} from 'sequelize';
 
 import {sequelize} from '../instances/mysql'
-
 import {Product} from '../models/Product'
 import {User} from '../models/User';
+
+
 
 export const paginaHome = async (req: Request, res: Response) => {
   try {
@@ -13,9 +15,58 @@ export const paginaHome = async (req: Request, res: Response) => {
       console.log("deu erro na conexão", error)
   }
 
-  let users = await User.findAll(); //users é um array de usuários
+  let users1 = await User.findAll({
+    attributes:{
+     // ['name', 'age']//filtragem
+     exclude: ['id'] //exclui da chamada
+    
+    }  ,
+    where:{
+      [Op.or]: [ // filtragem com "or" de valores
+      {age: 40},
+      {name: 'Renan'}
+    ]
+    } 
+    
+  }); 
+
+  let users2 = await User.findAll({
+    attributes:{
+     // ['name', 'age']//filtragem
+     exclude: ['id'] //exclui da chamada
+    
+    }  ,
+    where:{
+      age: 40 //filtragem direta do valor
+    } 
+    
+  });
+
+  let users3 = await User.findAll({
+    attributes:{
+     // ['name', 'age']//filtragem
+     exclude: ['id'] //exclui da chamada
+    
+    }  ,
+    where:{
+      age: {[Op.or]: [40, 30]}
+    } 
+    
+  });
+
+  let users4 = await User.findAll({
+    
+    where:{
+      age: {
+            [Op.gt]: 10
+           } //gt -> "maior que". gte -> "maior igual que". lt -> "menor que". lte -> Menor ou igual que. 
+    }        //between -> "entre os valores"(precisa de array de dois valores). not in -> "nao mostra essea valores"
+             //like -> "começam com" (usa-se o % para continuar, Ex: Re%, ou  %na%)
+    
+  });
   
-  console.log("USUARIOS: ", JSON.stringify(users));
+  
+  //console.log("USUARIOS: ", JSON.stringify(users));
     // res.send('Olá mundo');
 
     let showWelcome: boolean = false
@@ -44,6 +95,9 @@ export const paginaHome = async (req: Request, res: Response) => {
          'testando outra lista',
          'lista diferenciada'
      ],
-     users: users
+     users1: users1,
+     users2: users2,
+     users3: users3,
+     users4: users4
     });
 }
